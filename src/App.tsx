a2535ce170208955ad2d9c0a2dd04a27667294cc
MarcelArt <badman.islemon@gmail.com>
2025-03-20 21:48:15 +0700

    Reformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,37 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { TOTP } from 'totp-generator'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { TOTP } from 'totp-generator';
 
 function App() {
-  const [, setCount] = useState(0)
-  
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prev) => prev + 1); // Trigger a re-render by updating state
-    }, 30000); // 30 seconds
+	const [, setCount] = useState(0);
 
-    return () => clearInterval(interval); // Cleanup on unmount
-  }, []);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCount((prev) => prev + 1); // Trigger a re-render by updating state
+		}, 30000); // 30 seconds
 
+		return () => clearInterval(interval); // Cleanup on unmount
+	}, []);
 
+  const keyFromDb = 'BZN5MUZXOV5CDTTYEYQU3FJ5SBZBDLJU';
+	const { otp } = TOTP.generate(keyFromDb);
 
-  const { otp } = TOTP.generate('BZN5MUZXOV5CDTTYEYQU3FJ5SBZBDLJU');
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{otp}</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>{otp}</h1>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		</>
+	);
 }
 
-export default App
+export default App;
